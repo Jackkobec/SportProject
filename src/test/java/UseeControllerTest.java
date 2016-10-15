eb6e36fbba601f87a56a0f033e7cd0442e805452
jackkobec <jackkobec@gmail.com>
2016-10-15 13:46:51 +0300

    Version 1.708 Added UserController and realiosation of the structure: View -> UserController -> DAO -> DB Added test UserController method UserCreate;

---
+++ src/test/java/UseeControllerTest.java
@@ -0,0 +1,32 @@
+import controller.implement.UserControllerImpl;
+import controller.interfaces.UserControler;
+import model.app_db.AppDB;
+import model.app_db.UserDAO;
+import model.app_db.UserDAOimplement;
+import model.roles.Contacts;
+import model.roles.User;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Jack on 15.10.2016.
+ */
+public class UseeControllerTest {
+    AppDB appDB = new AppDB();
+    UserDAO userDAO = new UserDAOimplement(appDB);
+    UserControler userController = new UserControllerImpl(userDAO);
+
+
+    @Test
+    public void testUserCreate() {
+        User testUser = new User("Vasa", "7777777", new Contacts("vasa@mail.ru", "Vasa"));
+        //assertSame(new User("Vasa", "7777777"), userController.createUserCont(testUser));
+        assertEquals("Vasa", userController.createUserCont(testUser).getLogin());
+        assertEquals("7777777", userController.createUserCont(testUser).getPassword());
+        assertEquals("vasa@mail.ru", userController.createUserCont(testUser).getContacts().getEmail());
+        assertEquals("Vasa", userController.createUserCont(testUser).getContacts().getName());
+    }
+}
